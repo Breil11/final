8e119d8c069686dfe6e43696d48c73b48dcc5095
Breil11 <breilkom@gmail.com>
2023-05-15 19:52:06 +0200

    final

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Breil11/calculator2/super_calculator
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+//import "./super_calculator/cmd"
+import "github.com/Breil11/calculator2/super_calculator/cmd"
+
+func main() {
+	cmd.Execute()
+}
